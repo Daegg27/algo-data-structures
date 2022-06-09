78ba74e38538fc6d066ed2176efd1006f30cb4e8
Dalton Eggleston <egglestondalton@yahoo.com>
2022-06-08 17:03:22 -0700

    ingenuity at its finest

--- python/linked_list.py
+++ python/linked_list.py
@@ -1,19 +1,73 @@
 class LinkList:
-  # write your __init__ method here that should store a 'head' value which the first Node in the LinkedList and a 'length' value which is the total number of Nodes in the LinkedList
+    def __init__(self, head = None):
+      # write your __init__ method here that should store a 'head' value which the first Node in the LinkedList and a 'length' value which is the total number of Nodes in the LinkedList
+      self.length = 0
+      self.head = head
+    
+  
+    def add(self, data):
+      # write your code to ADD an element to the Linked List
+      if self.head == None:
+        self.head = Node(data)
+        self.length += 1
+        return
+        
+      current = self.head
+    
+      while current.next != None:
+        current = current.next
+      self.length += 1
+      current.next = Node(data)
 
-  def add(self, data):
-    # write your code to ADD an element to the Linked List
-    pass
+      
+    
 
-  def remove(self, data):
-    # write your code to REMOVE an element from the Linked List
-    pass
+    def remove(self, data):
+      # write your code to REMOVE an element from the Linked List
+      if head == None:
+        return
+      
+      if head.data == data:
+        head = head.next
+        length -= 1
 
-  def get(self, element_to_get):
-    # write you code to GET and return an element from the Linked List
-    pass
+      current = self.head
+      while current.next != None:
+        if current.next.data == data:
+          current.next = current.next.next
+          length -= 1
+          return
+        current = current.next
+
+    def get(self, element_to_get):
+      # write you code to GET and return an element from the Linked List
+      if self.head == None:
+        return "No data available"
+      if self.head.data == element_to_get:
+        return element_to_get
+
+      current = self.head
+      while current.next != None:
+        if current.next.data == element_to_get:
+          return element_to_get
+        else:
+          current = current.next
+      
 
 # ----- Node ------
 class Node:
   # store your DATA and NEXT values here
-  pass
+  
+    def __init__(self, data, next = None):
+      
+      self.data = data
+      self.next = next
+
+# Testing Add method
+ls = LinkList()
+print(ls.length)
+ls.add(2)
+ls.add(5)
+ls.add(6)
+ls.add(10)
+print(ls.get(5))
